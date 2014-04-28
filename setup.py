e00841ed9d42c5e1ea0c1b3fb6941c2a2fb693d7
Andy Raines <andrew.raines@gmail.com>
2014-04-29 00:03:43 +0100

    Setting up package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='pylightwaverf',
+	version='0.1',
+	description='Python library for communication with LightWaveRF products (via the WiFi Link)',
+	url='https://github.com/r4abigman/pylightwaverf',
+	author='Andy Raines',
+	author_email='',
+	license='MIT',
+	packages=['pylightwaverf'],
+	zip_safe=False
+)
